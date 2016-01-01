f1ca25d4187c55e93c2c92d2848e5b2600ebe5d0
valik-pc <valik.v1per@gmail.com>
2016-01-01 02:28:35 +0200

    Initial commit

---
+++ routes/init.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	mgo "gopkg.in/mgo.v2"
+)
+
+type database struct {
+	host     string
+	database string
+	username string
+	password string
+	port     string
+}
+
+var databaseConfig database
+
+func Init() (*mgo.Session, error) {
+	file, err := ioutil.ReadFile("../config/database.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var databaseConfig database
+
+	err = json.Unmarshal(file, databaseConfig)
+
+	session, err := mgo.Dial(databaseConfig.host)
+
+	if err != nil {
+		panic(err)
+	}
+
+	session.DB(databaseConfig.database)
+
+	return session, nil
+}
